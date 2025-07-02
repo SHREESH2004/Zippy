10849adafc9282ac555c4b3d19c1c24bc1ace5b4
Shreesh <shreeshsanyal@gmail.com>
2025-07-02 16:17:40 +0530

    added filteroptions

---
+++ client/vite-project/src/pages/admin/index.js
@@ -0,0 +1,16 @@
+export const filteroption = {
+    category: [
+        { id: "men", label: "Men" },
+        { id: "woman", label: "Woman" },
+        { id: "kids", label: "Kids" },
+        { id: "accessories", label: "Accessories" },
+        { id: "footwear", label: "Footwear" },
+    ],
+    brand: [
+        { id: "nike", label: "Nike" },
+        { id: "adidas", label: "Adidas" },
+        { id: "puma", label: "Puma" },
+        { id: "zara", label: "Zara" },
+        { id: "h&m", label: "H&M" },
+    ],
+};
