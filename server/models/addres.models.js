015df9ba92211eae628e5475cadc5407da196a16
Shreesh <shreeshsanyal@gmail.com>
2025-07-06 12:42:41 +0530

    address Schema made

---
+++ server/models/addres.models.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+const addressSchema = new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    pincode: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: true
+    },
+    phoneno: {
+        type: Number,
+        required: true
+    },
+    notes: {
+        type: String,
+        default: ''
+    },
+}, { timestamps: true})
+const Address = mongoose.model('Address', addressSchema);
+export default Address;
