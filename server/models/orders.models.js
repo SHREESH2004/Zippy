08b4b8229172665a7eac5b268b95704b041dc788
Shreesh <shreeshsanyal@gmail.com>
2025-07-07 02:35:35 +0530

    orderschema made

---
+++ server/models/orders.models.js
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+
+  cart: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Cart',
+    required: true
+  },
+
+  address: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Address',
+    required: true
+  },
+
+  shippingAddress: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Address',
+    required: true
+  },
+
+  paymentMethod: {
+    type: String,
+    enum: ['Cash on Delivery', 'Online Payment'],
+    required: true,
+  },
+
+  paymentStatus: {
+    type: String,
+    enum: ['Pending', 'Paid'],
+    default: 'Pending',
+  },
+
+  shippingCost: { type: Number, default: 0 },
+  discount: { type: Number, default: 0 },
+  tax: { type: Number, default: 0 },
+  totalAmount: { type: Number, required: true },
+
+  status: {
+    type: String,
+    enum: ['Pending', 'Confirmed', 'Shipped', 'Delivered', 'Cancelled'],
+    default: 'Pending',
+  },
+}, {
+  timestamps: true,
+});
+
+export default mongoose.model('Order', orderSchema);
